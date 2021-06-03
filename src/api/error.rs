439f3be95e95554f7d1b0a45ef42b6325ea1ee94
Rodrigo Sanabria <krosf@muchobien.dev>
2021-06-03 17:27:53 +0200

    fix(deps): remove serde_urlencoded

--- src/api/error.rs
+++ src/api/error.rs
@@ -12,13 +12,6 @@ struct Status {
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum BodyError {
-    /// Body data could not be serialized from form parameters.
-    #[error("failed to URL encode form parameters: {}", source)]
-    UrlEncoded {
-        /// The source of the error.
-        #[from]
-        source: serde_urlencoded::ser::Error,
-    },
     #[error("failed to encode body parameters: {}", source)]
     JsonEncodee {
         /// The source of the error.
